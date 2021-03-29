8c9d6a3868da8c424e805587b65e9a54db075db7
Julia <mjulia1794@gmail.com>
2021-03-29 16:44:12 +0300

    Updates

---
+++ precache-manifest.099afd64a940ea876efadf0669062ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd730aac6201623baa633acaf9fc414",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a060e82f388e9b0d6b57",
+    "url": "./static/css/main.b2c8fac9.chunk.css"
+  },
+  {
+    "revision": "9cb276670027b79266c7",
+    "url": "./static/js/2.bafbf7af.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bafbf7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a060e82f388e9b0d6b57",
+    "url": "./static/js/main.b285e669.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
